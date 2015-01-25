f2732e48b0dbf661d1baa665137436ffc8e766c3
David Cardona <dcardon@gmail.com>
2015-01-25 16:53:04 -0500

    R Programming - Assignment 2 Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code follows the skeleton of the example, only that it uses the function
+## solve to calculate the inverse of the matrix, also added a message indicating when
+## was the matrix initialized
 
-## Write a short comment describing this function
+## This functions creates a closure that contains the data about the matrix,
+## in its first call, it doesn't store the inverse, note that when a new
+## matrix is set, the cached inverse is deleted
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    print("Initalizing matrix...")
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(i) inv <<- i
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function takes the previous closure as an argument and
+## checks if the inverse was already calculated, if so, it returns;
+## if the inverse isn't cached it solves the matrix and stores the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    print("Getting cached inverse...")
+    return(i)
+  }
+  print("Solving matrix and caching...")
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
